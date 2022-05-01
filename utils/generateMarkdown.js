ef636bcf8f61629375a11a736f342793a9f05ec1
bshor <bshort985@gmail.com>
2022-05-01 13:23:11 -0400

    finished license functions

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,82 +1,114 @@
 const fs = require('fs');
 const inquirer = require('inquirer');
 const index = require('../index.js');
-let license = "";
-let licenseLink = "";
+
 
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {
 
-}
+function renderLicenseBadge(license) {
+  let badge = "";
+  if (license === "MIT"){
+    badge = "https://img.shields.io/npm/l/inquirer";
+  } else if (license === "Apache License 2.0"){
+    badge = "https://img.shields.io/badge/license-Apache2.0-brightgre"
+  }else if (license === "GNU AGPLv3"){
+    badge = "https://img.shields.io/badge/license-GNUAGPLv3-orange"
+  }else if (license === "Unlicense"){
+    badge = "https://img.shields.io/badge/license-Unlicense-lightgrey"
+  }
+  console.log("badge is " + badge);
+  console.log("license is " + license);
+  return badge;
+};
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+  let licenseLink = "";
+  if(license === "MIT") {
+    licenseLink = "https://choosealicense.com/licenses/mit/"
+  } else if (license === "Apache 2.0") {
+    licenseLink = "https://choosealicense.com/licenses/apache-2.0/"
+  } else if (license === "GNU AGPLv3") {
+    licenseLink = "https://choosealicense.com/licenses/agpl-3.0/"
+  } else if (license === "Unlicense") {
+    licenseLink = "https://choosealicense.com/licenses/unlicense/"
+  }
+  return licenseLink;
+};
+
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) {
+  let licenseSection = ''
+  if(license === 'None') {
+    licenseSection = ''
+  } else {
+    licenseSection =
+    `License: ${license} `
+  }
+  return licenseSection;
+}
 
 //TODO: Create a function to generate markdown for README
 
 function generateMarkdown(answer) {
 
-  const  data = {}
-  return `# ${answer.title}
+  return `
+  
+  # ${answer.title}
 
-          ## Description 
+  ## ${renderLicenseSection(answer.license)} ### ${renderLicenseBadge(answer.license)}
+  ### ${renderLicenseLink(answer.license)}
 
-          ${answer.description}
+  ## Description 
+  ### ${answer.description}
 
-          ##Motivation 
-          ${answer.motovation}
+  ## Motivation 
+  ### ${answer.motovation}
 
-          ##Problem
-          ${answer.problem}
+  ## Problem
+  ### ${answer.problem}
 
-          ##Lessons Learned
-          ${answer.lesson}
+  ##Lessons Learned
+  ### ${answer.lesson}
 
-          ## Table of Contents:
-          ###  * [Installation](#installation)
-          ###  * [Usage](#usage)
-          ###  * [License](#license)
-          ###  * [Contributors](#contributors)
-          ###  * [Tests](#tests)
-          ###  * [Questions](#questions)
+  ## Table of Contents:
+  ###  * [Installation](#installation)
+  ###  * [Usage](#usage)
+  ###  * [License](#license)
+  ###  * [Contributors](#contributors)
+  ###  * [Tests](#tests)
+  ###  * [Questions](#questions)
 
-          ## Installation
+  ## Installation
 
-          ### ${answer.installation}
+  ### ${answer.installation}
 
-          ## Usage
+  ## Usage
 
-          ### ${answer.usage}
+  ### ${answer.usage}
 
-          ## License
+  ### Read the license here (Link)
 
-          ### ${answer.license}
+  ## Contributors
 
-          ### Read the license here (Link)
+  ### ${answer.contributers}
 
-          ## Contributors
+  ## Tests
 
-          ### ${answer.contributers}
+  ### ${answer.test}
 
-          ## Tests
+  ## Questions
 
-          ### ${answer.test}
-
-          ## Questions
-
-          ### Github Repo ${answer.github}
-          ### If you have any question please don't hesitate to reach out via email. My email is ${answer.emal}
+  ### Github Repo ${answer.github}
+  ### If you have any question please don't hesitate to reach out via email. My email is ${answer.email}
 
           
 
 `;
-console.log(generateMarkdown)
 
 }
 
